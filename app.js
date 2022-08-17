e9bfc9d4a8969a0d2a2d67bddc3f135cea140d7e
Fatih Sarıkaya <f.sarikaya@gmail.com>
2022-08-17 22:34:59 +0300

    işlemler

---
+++ app.js
@@ -0,0 +1,20 @@
+// let note= +prompt("Not giriniz...")
+
+// while(note<0||note>100){
+//     note = +prompt("Tekrar Note Giriniz...")
+//     console.log(`notunuz ${note} `)    
+// }
+// alert(`Notunuz:${note}`);
+
+        // Do While
+
+
+// let not;
+// do {
+//     not = +prompt("Lütfen 0-100 arasında bir not giriniz:")
+//     if (not<0 || not>100){
+//         alert("not 0-100 arasıda olmalıdır")
+//     }
+// } while (not<0 || not>100);
+
+// console.log("Girdiginiz Not:",not);
